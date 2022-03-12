344f007ee9b2bd4d1e55feb97bbe9a2c89b9647c
Chris Ting <kkting@students.cs.ubc.ca>
2022-03-12 00:51:10 -0800

    Added buttons to welcome page;

--- src/main/ui/BlackjackUI.java
+++ src/main/ui/BlackjackUI.java
@@ -7,22 +7,53 @@ import java.awt.*;
 
 class BlackjackUI extends JFrame {
 
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
     private BlackjackGame game;
-    private MenuUI menu;
+    private JFrame mainFrame;
+    private ImagePanel image;
+    private JLabel message;
 
     // EFFECTS: sets up window in which Blackjack game will be played
     public BlackjackUI() {
-        super("Blackjack Game Simulator");
-        game = new BlackjackGame();
-        menu = new MenuUI(game);
+        this.game = new BlackjackGame();
+        this.image = new ImagePanel(new ImageIcon("./images/menuBackground.jpg").getImage());
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setResizable(false);
+        mainFrame = new JFrame("Blackjack Game Simulator");
+        mainFrame.add(image);
+        welcomeMessage();
+        addButtons();
 
-        add(menu);
-        pack();
+        mainFrame.setSize(WIDTH, HEIGHT);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+        mainFrame.setVisible(true);
         centreOnScreen();
-        setVisible(true);
+    }
+
+    public void welcomeMessage() {
+        JLabel message = new JLabel("Welcome to Blackjack!");
+        message.setBounds(80, 100, 500, 100);
+        image.add(message);
+    }
+
+    public void addButtons() {
+        JButton playBtn = new JButton("Play");
+        JButton loadBtn = new JButton("Load");
+        JButton quitBtn = new JButton("Quit");
+
+        playBtn.setBounds(80,250,100,50);
+        loadBtn.setBounds(80,325,100,50);
+        quitBtn.setBounds(80,400,100,50);
+
+        playBtn.setFont(new Font("Avenir", Font.BOLD, 25));
+        loadBtn.setFont(new Font("Avenir", Font.BOLD, 25));
+        quitBtn.setFont(new Font("Avenir", Font.BOLD, 25));
+
+        image.add(playBtn);
+        image.add(loadBtn);
+        image.add(quitBtn);
     }
 
     // Centres frame on desktop
---
+++ src/main/ui/ImagePanel.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImagePanel extends JComponent {
+
+    private Image image;
+
+    public ImagePanel(Image img) {
+        this.image = img;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, null);
+    }
+}
